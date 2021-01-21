a8d3ed5bffb063cae73feed9d3eb655b273eacae
KL <rhymedevkl@gmail.com>
2021-01-20 22:10:57 -0500

    Checking out the Jinja portion

--- app.py
+++ app.py
@@ -1,10 +1,22 @@
-from flask import Flask
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello World!"
+    return render_template('home.html')
+
+@app.route('/checklist')
+def checklist():
+    return render_template('checklist.html')
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def server_error(e):
+    return render_template('500.html'), 500
 
 if __name__ == '__main__':
     app.run()
